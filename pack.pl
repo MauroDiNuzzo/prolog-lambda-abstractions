7c3705978e9c209e2b0a62e7ce12e03c4a77ea53
Mauro DiNuzzo <44751228+MauroDiNuzzo@users.noreply.github.com>
2018-11-05 16:30:29 +0100

    Add files via upload

---
+++ pack.pl
@@ -0,0 +1,6 @@
+name(lambda_abstractions).
+title('Lambda abstractions').
+version('0.1.1').
+keywords([lambda]).
+author('Mauro DiNuzzo', 'mauro.dinuzzo@prologserverpages.com').
+download('https://github.com/maurodinuzzo/prolog-lambda-expressions/raw/master/lambda_abstractions-*.zip').
